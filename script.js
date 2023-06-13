9c15d80be0e4157a00560b18b09be5a997468115
kurtpagal <zja641@usask.ca>
2023-06-13 15:02:11 -0600

    Completed HTML and worked on some JS

---
+++ script.js
@@ -0,0 +1,38 @@
+let runningSum = 0;
+let buffer = '0';
+let prevOperator; 
+
+const screen = document.querySelector('.screen'); 
+const calcButton = document.querySelector(".calc-buttons");
+
+calcButton.addEventListener('click', function(event) {
+    let opt = event.target.innerText;  
+
+    if (isNaN(opt)) {
+        checkSymbol(opt);
+    } else {
+        handleNumber(opt); 
+    }
+    screen.innerText = buffer; 
+}, false);
+
+function checkSymbol(sym) {
+    switch (sym) {
+        case 'Clear':
+            runningSum = 0;
+            buffer = '0';
+            break; 
+
+        default:
+            alert("ERROR!");
+            break;
+    }
+}
+
+function handleNumber(stringNumber) {
+    if (stringNumber === '0') {
+        buffer = stringNumber;
+    } else {
+        buffer += stringNumber; 
+    }
+} 
