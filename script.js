f8d1805081dcea60401e433f122497c4a79cf6de
Kurt P <kurtpagal@gmail.com>
2023-06-14 13:41:40 -0600

    Completed the +/- option

--- script.js
+++ script.js
@@ -19,24 +19,41 @@ calcButton.addEventListener('click', function(event) {
 function checkSymbol(sym) {
     switch (sym) {
         case 'Clear':
+            // alert("It reached here")
             runningSum = 0;
             buffer = '0';
             break; 
         case 'Del': 
+            // alert("It reached here")
             if (buffer.length === 1) {
                 buffer = '0'; 
             } else {
-                buffer = buffer.toString(0, buffer.length - 1);
+                buffer = buffer.substring(0, buffer.length - 1);
             }
             break;
+        case '+/-':
+            // alert("It reached here");
+            checkNumber(parseInt(buffer));
+            break; 
+        case '+':
+        case '-':
         default:
             alert("ERROR!");
             break;
     }
 }
 
+function checkNumber(inputNumber) {
+    if (inputNumber === 0) {
+        return;
+    } else {
+        var newNum = inputNumber * -1; 
+        buffer = newNum.toString(); 
+    }
+}
+
 function handleNumber(stringNumber) {
-    if (stringNumber === '0') {
+    if (stringNumber === "0") {
         buffer = stringNumber;
     } else {
         buffer += stringNumber; 
